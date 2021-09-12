962595583a6023286a705ff2a56ca3c5a38b8884
Gilberto Pineda <gilbertopinedaf262@hotmail.com>
2021-09-12 01:20:17 -0500

    cambio de color de las lineas

--- funcion.js
+++ funcion.js
@@ -1,7 +1,7 @@
 context = document.getElementById("elTablero").getContext("2d");
 context.beginPath();
 context.moveTo(150,20);
-context.strokeStyle = 'green';
+context.strokeStyle = 'red';
 context.lineTo(50,90);
 //context.lineTo(maneja eje x, maneja eje y); 
 context.lineTo(250,90);
